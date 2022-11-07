64a78b1fbc58c1dcccca487ad92e0342752a42fd
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-11-07 21:27:25 +0700

    done: calc runtime and loop style

--- source.c
+++ source.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
 
-    int* arr = malloc(sizeof(int)*50);
+    int sizeNumber = 1600000000;
+    int* arr = malloc(sizeof(int)*sizeNumber);
+    int i;
+    int sum;
+
+    clock_t begin, end;
+    double time_spent = 0.0;
+
 
     // generate number
-    for(int i = 0; i < 50; i++){
+    for(i = 0; i < sizeNumber; i++){
         arr[i] = i+1;
     }
 
 
     // loop style
-    
+    begin = clock();
+    for(i = 0; i < sizeNumber; i++){
+        sum+=arr[i];
+    }
+    end = clock();
+    time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("result: %d / time: %f", sum, time_spent);
 
     // math big brain style
 
