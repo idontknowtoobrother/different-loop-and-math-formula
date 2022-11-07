0a062c698ca4088cc78ee57377a63363948348ca
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-11-07 22:27:44 +0700

    stored to my git <3

--- source.c
+++ source.c
@@ -4,30 +4,37 @@
 
 int main(){
 
-    int sizeNumber = 1600000000;
-    int* arr = malloc(sizeof(int)*sizeNumber);
-    int i;
-    double sum;
+    int sizeNumber = 99999999;
 
-    clock_t begin, end;
-    double time_spent = 0.0;
+    int *arr = malloc(sizeof(int)* sizeNumber);
+    double sum = 0.0, math_style_sum = 0.0;
 
+    clock_t begin, end, begin_math, end_math;
+    double time_spent = 0.0, math_time_spent = 0.0;
 
     // generate number
-    for(i = 0; i < sizeNumber; i++){
+    for(int i = 0; i < sizeNumber; i++){
         arr[i] = i+1;
     }
 
 
     // loop style
     begin = clock();
-    for(i = 0; i < sizeNumber; i++){
-        sum+=arr[i];
+    for(int i = 0; i < sizeNumber; i++){
+        sum+= arr[i];
     }
     end = clock();
     time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("result: %d / time: %f", sum, time_spent);
+    printf("result: %.2f / time: %f", sum, time_spent);
 
     // math big brain style
+    begin_math = clock();
+    double firstNum = arr[0];
+    double lastNum = arr[sizeNumber-1];
+    double calcT = lastNum-firstNum+1;
+    math_style_sum = (firstNum+lastNum)*calcT/2;
+    end_math = clock();
+    math_time_spent += (double)(begin_math - end_math) / CLOCKS_PER_SEC;
+    printf("\nresult: %.2f / time: %f", math_style_sum, math_time_spent);
 
 }
